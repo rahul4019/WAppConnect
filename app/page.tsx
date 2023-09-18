e850d3cc3ea845211abbc64d077548bd2e122086
rahul4019 <rahulkashyap4019@gmail.com>
2023-09-18 23:25:04 +0530

    Create user, chat and message models

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import axios from 'axios';
+
+const getData = async () => {
+  const { data } = await axios.get(`${process.env.URL}/api`);
+  console.log(data);
+};
+
+export default async function Home() {
+  await getData();
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
   );
---
+++ models/user.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        'https://res.cloudinary.com/rahul4019/image/upload/v1695059345/avatar_xf4t46.png',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
