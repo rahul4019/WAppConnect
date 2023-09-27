325c471aba0cb1713fb1cc695665f425caa669d0
rahul4019 <rahulkashyap4019@gmail.com>
2023-09-27 08:05:58 +0530

    Add header of mychat section

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,14 +1,19 @@
 import { store } from '@/store';
 import Container from '@/components/ui/container';
+import MyChatHeader from '@/components/ui/MyChatHeader';
 
 export default async function Chats() {
   const userInfo = store.getState().user.userInfo;
 
   return (
-    <div className="flex min-h-screen  min-w-full flex-col bg-red-200">
+    <div className="flex min-h-screen  min-w-full flex-col bg-red-200 p-4">
       <Container>
         <div className="flex w-full justify-between">
-          <div className="hidden flex-1 bg-blue-200 md:flex">my chats</div>
+          {/* My chats section */}
+          <div className="hidden flex-1 bg-blue-200 md:flex">
+            <MyChatHeader />
+          </div>
+          {/* Chat box section */}
           <div className="flex-1 bg-green-200">chat box</div>
         </div>
       </Container>
---
+++ components/ui/MyChatHeader.tsx
@@ -0,0 +1,45 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { MessageSquarePlus, MoreVertical } from 'lucide-react';
+
+const MyChatHeader = () => {
+  return (
+    <div className="flex w-full items-center justify-between bg-gray-200 p-3">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className="bg-blue-20 flex gap-5 px-2">
+        {/* message icon */}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <MessageSquarePlus />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>New chat</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        {/* menu icon */}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <MoreVertical />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Menu</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
+};
+
+export default MyChatHeader;
