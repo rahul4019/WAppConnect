2c35a11676d40720d5f81b9fb32313255265be97
rahul4019 <rahulkashyap4019@gmail.com>
2023-10-25 05:51:28 +0530

    Fix deployment failure due to classname in sheet component

--- components/ui/ChatBoxSection.tsx
+++ components/ui/ChatBoxSection.tsx
@@ -2,20 +2,22 @@
 
 import { useState } from 'react';
 import ChatBoxSectionHeader from './ChatBoxSectionHeader';
+import ChatInput from './ChatInput';
 
 export default function ChatBoxSection() {
   const [chatSelected, setChatSelected] = useState(true);
 
   return (
-    <div className='flex flex-col bg-green-200 w-full h-full'>
+    <div className="flex h-full w-full flex-col bg-green-200">
       {!chatSelected ? (
         <div className="text-3xl font-light text-gray-400">
           Click on a user to start a chat
         </div>
       ) : (
-        <div>
+        <div className="flex flex-col items-baseline justify-between">
           <ChatBoxSectionHeader />
           chat is selected
+          <ChatInput />
         </div>
       )}
     </div>
