f1f142646a6d22330a22920d9dce5792b66ce70b
rahul4019 <rahulkashyap4019@gmail.com>
2023-10-26 06:54:15 +0530

    Add background to ChatBoxSection

--- components/ui/ChatBoxSection.tsx
+++ components/ui/ChatBoxSection.tsx
@@ -3,20 +3,31 @@
 import { useState } from 'react';
 import ChatBoxSectionHeader from './ChatBoxSectionHeader';
 import ChatInput from './ChatInput';
+import background from '../../public/background.jpg';
+import Image from 'next/image';
 
 export default function ChatBoxSection() {
   const [chatSelected, setChatSelected] = useState(true);
 
   return (
-    <div className="flex h-full w-full flex-col bg-green-200">
+    <div className="relative flex h-full w-full flex-col">
+      <Image
+        src={background}
+        alt="background"
+        fill
+        priority
+        className="-z-1"
+        placeholder="blur"
+        style={{ objectFit: 'cover' }}
+      />
       {!chatSelected ? (
         <div className="text-3xl font-light text-gray-400">
           Click on a user to start a chat
         </div>
       ) : (
-        <div className="flex flex-col items-baseline justify-between  h-full">
+        <div className="flex h-full flex-col items-baseline  justify-between">
           <ChatBoxSectionHeader />
-          <div>chat is selected</div>
+          <div className="z-10 text-white">chat is selected</div>
           <ChatInput />
         </div>
       )}
--- components/ui/ChatInput.tsx
+++ components/ui/ChatInput.tsx
@@ -4,10 +4,10 @@ import { Plus, Mic } from 'lucide-react';
 
 export default function ChatInput() {
   return (
-    <div className="flex h-14 w-full items-center justify-center gap-4 bg-gray-100 px-4">
-      <Plus />
+    <div className="flex h-14 w-full items-center justify-center gap-4 bg-gray-100 px-4 z-10">
+      <Plus className='cursor-pointer'/>
       <Input type="text" placeholder="Type a message"></Input>
-      <Mic />
+      <Mic className='cursor-pointer'/>
     </div>
   );
 }
